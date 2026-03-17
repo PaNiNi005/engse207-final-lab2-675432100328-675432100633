75ac9e4650bba4e3dbecb78dcb55f91447a97d01
PaNiNi005 <kanokkorn19733@gmail.com>
2026-03-17 22:19:26 +0700

    edit profile

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,5 @@
 window.APP_CONFIG = {
   AUTH_URL: 'https://auth-service-production-ee6c.up.railway.app', // เปลี่ยนเป็น URL ของคุณ
   TASK_URL: 'https://task-service-production-d541.up.railway.app', // เปลี่ยนเป็น URL ของคุณ
-  USER_URL: 'https://user-service-production-801b.up.railway.app'  // เปลี่ยนเป็น URL ของคุณ
+  USER_URL: 'https://user-service-production-801b.up.railway.app/api'  // เปลี่ยนเป็น URL ของคุณ
 };
